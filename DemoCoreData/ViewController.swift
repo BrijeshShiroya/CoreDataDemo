d5c9f4fa6bf6f48f93f0d2507b2a71ac45cd6cdb
Brijesh <brijesh@creolestudios.com>
2018-05-28 08:06:16 +0530

    Initial commit

--- DemoCoreData/ViewController.swift
+++ DemoCoreData/ViewController.swift
@@ -7,9 +7,11 @@
 //
 
 import UIKit
-
+import CoreData
+let AppDelegateObj = UIApplication.shared.delegate as! AppDelegate
 class ViewController: UIViewController {
 
+    var people:[NSManagedObject] = [NSManagedObject]()
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +22,18 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func btnSaveClicked(_ sender: UIButton) {
+        let managedContext  = AppDelegateObj.persistentContainer.viewContext
+        let entity = NSEntityDescription.entity(forEntityName: "Person", in: managedContext)
+        let person:NSManagedObject = NSManagedObject.init(entity: entity!, insertInto: managedContext)
+        person.setValue("Brijesh", forKey: "name")
+        do{
+            try managedContext.save()
+        }catch let error as NSError{
+            print("Could not save. \(error), \(error.userInfo)")
+        }
+        
+    }
+    
 }
 
